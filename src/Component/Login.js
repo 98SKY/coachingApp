7f4238d223921243d8b9751eb2b906fe582d9e11
SUNIL KUMAR <you@example.com>
2024-02-23 00:07:36 +0530

    loginPageDone

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React from 'react';
 import Reset from './Global';
 import '../global.css';
-const Login = () => {
-  Reset(5,2);
-  return (
-    <div className="padding-all">
-      <p>Learn login</p>
-      <p>Component content</p>
-    </div>
-  )
-}
+import './login.css';
 
-export default Login
+const Login = () => {
+  Reset(5, 2);
+  return (
+    <div className='padding-all'>
+      <div className="header">Login</div>
+      <div className="body">
+        <img src="/logo192.png" alt="Logo" className='body-img' />
+        <div className="login-box">
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button>Login</button>
+          <div className="create-account">Create account</div>
+          <div className="forgot-password">Forgot password?</div>
+        </div>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
+};
+
+export default Login;
