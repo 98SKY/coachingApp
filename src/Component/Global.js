a77f79706f708f1e45b4f5e0b76ec446f4f28194
SUNIL KUMAR <you@example.com>
2024-02-22 22:40:22 +0530

    first stage done

---
+++ src/Component/Global.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Global = () => {
+  const Reset = (x,y) =>{
+    let result = x+y;
+  };
+  const check = () =>{};
+}
+
+export default Global
--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Reset from './Global';
+import '../global.css';
 const Login = () => {
+  Reset(5,2);
   return (
-    <div>
+    <div className="padding-all">
       <p>Learn login</p>
+      <p>Component content</p>
     </div>
   )
 }
