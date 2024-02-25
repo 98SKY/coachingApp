5afa2c460a69f4ae8eceb1b1ed4f9c897e063032
SUNIL KUMAR <you@example.com>
2024-02-25 22:27:46 +0530

    initial dashBoard setup

--- src/Component/DashBoard.js
+++ src/Component/DashBoard.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { useState,useEffect } from 'react';
-import { Link,useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../global.css';
+import './dashBoard.css'
+
 
 const DashBoard = () => {
-  
   const navigate = useNavigate();
   const [backButtonCount, setBackButtonCount] = useState(0);
+  const [isSideDrawerOpen, setIsSideDrawerOpen] = useState(false);
 
   useEffect(() => {
     const handleBeforeUnload = (event) => {
-      if (backButtonCount >= 2) { // Change this threshold as needed
+      if (backButtonCount >= 2) {
         event.preventDefault();
         navigate('/');
       } else {
@@ -24,11 +26,36 @@ const DashBoard = () => {
     };
   }, [navigate, backButtonCount]);
 
+  const toggleSideDrawer = () => {
+    setIsSideDrawerOpen(!isSideDrawerOpen);
+  };
+
   return (
-    <div>
-      <p>DashBoard</p>
+    <div className='wrapper'>
+      <div className='padding-all'>
+        <div className="header-dashBoard">
+          <button className="menu-button" onClick={toggleSideDrawer}>
+            Menu
+          </button>
+          <div className='dashboard-heading'>DashBoard</div>
+        </div>
+        <div className='dashBoardBody'>body</div>
+        {isSideDrawerOpen && <SideDrawer />}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+const SideDrawer = () => {
+  return (
+    <div className="side-drawer">
+      <ul>
+        <li><a href="#">Link 1</a></li>
+        <li><a href="#">Link 2</a></li>
+        <li><a href="#">Link 3</a></li>
+      </ul>
+    </div>
+  );
+};
 
 export default DashBoard
