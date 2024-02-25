ba4e97805d9cc3951bd57f01a869c4432755172a
SUNIL KUMAR <you@example.com>
2024-02-25 17:34:11 +0530

    field value handled

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import Reset from './Global';
 import '../global.css';
 import './login.css';
-import { Route } from './Route';
 import { Link,useNavigate } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 const Login = () => {
 
   const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleLogin = () => {
     const userExists = false;
     // Check your condition here
+    console.log('Username:', username);
+    console.log('Password:', password);
 
     if (userExists) {
       // Navigate to the dashboard
@@ -29,8 +35,11 @@ const Login = () => {
       <div className="body">
         <img src="/logo192.png" alt="Logo" className='body-img' />
         <div className="login-box">
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
+          <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <div className="password-input">
+              <input type={showPassword ? "text" : "password"} placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              <i className={`fa ${showPassword ? "fa-eye-slash" : "fa-eye"} password-icon`} onClick={() => setShowPassword(!showPassword)}></i>
+            </div>
           <button onClick={handleLogin}>Login</button>
           <div>
           <div className="create-account"><Link to={"SignUp"}>Create account</Link></div>
