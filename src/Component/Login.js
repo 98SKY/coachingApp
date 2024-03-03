0e51c71e53c179eb3eb2016f88aa00a06305b262
SUNIL KUMAR <you@example.com>
2024-03-03 14:10:23 +0530

    login page got language

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -4,6 +4,7 @@ import '../global.css';
 import './login.css';
 import { Link,useNavigate } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { useLocation } from "react-router-dom";
 
 
 const Login = () => {
@@ -12,6 +13,10 @@ const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const language = params.get("language");
+  const userType = params.get("userType");
 
   const handleLogin = async () => {
     if (username.trim() === '' || password.trim() === '') {
@@ -57,6 +62,14 @@ const Login = () => {
     }
   }, []);
 
+  const handleCreateAccount = () => {
+    if (userType !== "institute") {
+      alert("Contact your institute to create an account.");
+    } else {
+      navigate("/SignUp");
+    }
+  };
+
   return (
     <div className='wrapper'>
     <div className='padding-all'>
@@ -64,14 +77,14 @@ const Login = () => {
       <div className="body">
         <img src="/logo192.png" alt="Logo" className='body-img' />
         <div className="login-box">
-          <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <input type="text" placeholder={userType === "institute" ? "Institute ID" : "Username"} value={username} onChange={(e) => setUsername(e.target.value)} />
           <div className="password-input">
               <input type={showPassword ? "text" : "password"} placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
               <i className={`fa ${showPassword ? "fa-eye-slash" : "fa-eye"} password-icon`} onClick={() => setShowPassword(!showPassword)}></i>
             </div>
           <button onClick={handleLogin}>Login</button>
           <div>
-          <div className="create-account"><Link to={"SignUp"}>Create account</Link></div>
+          <div className="create-account" onClick={handleCreateAccount}>Create account</div>
           <div className="forgot-password"><Link to={"ForgotPass"}>Forgot password?</Link></div>
           </div>
         </div>
