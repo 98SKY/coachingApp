b9a7f1a6c5ac187c3b383d79acea7b1031c4ba07
SUNIL KUMAR <you@example.com>
2024-03-04 16:15:18 +0530

    create page generated

--- src/Component/SignUp.js
+++ src/Component/SignUp.js
@@ -1,18 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 import '../global.css';
-import './dashBoard.css'
+import './signUp.css';
 
 const SignUp = () => {
-  return (
-    <div className='wrapper'>
-    <div className='padding-all'>
-        <div className='header'>Register My self</div>
-        <div className='body'>body</div>
-        <div className='footer'></div>
-    </div>
-  </div>
-  )
-}
+  const [formData, setFormData] = useState({
+    instituteName: '',
+    phoneNumber: '',
+    emailId: '',
+  });
+  const [formValid, setFormValid] = useState(false);
 
-export default SignUp
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form validation here
+    if (formData.instituteName && formData.phoneNumber && formData.emailId) {
+      setFormValid(true);
+    }
+  };
+
+  return (
+    <div className='myWrapper'>
+      <div className='myPadding-all'>
+        <div className='header'>Register Myself</div>
+        <form onSubmit={handleSubmit}>
+          <div className='body'>
+            <input
+              type='text'
+              name='instituteName'
+              placeholder='Enter institute name 1'
+              value={formData.instituteName}
+              onChange={handleChange}
+              required
+              className='myInput-field'
+            />
+            <input
+              type='tel'
+              name='phoneNumber'
+              placeholder='Enter phone number 2'
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              required
+              className='myInput-field'
+            />
+            <input
+              type='email'
+              name='emailId'
+              placeholder='Enter email ID 3'
+              value={formData.emailId}
+              onChange={handleChange}
+              required
+              className='myInput-field'
+            />
+            {/* <div className="buttonWrapper"> */}
+            <button type='submit' onClick={handleChange}>
+              Save
+            </button>
+            {/* </div> */}
+          </div>
+          <div className='footer'>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
